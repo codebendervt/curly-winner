e91099d35a2934e853ce194c4cd10504ea637c87
codebender <lionelmbankeu@gmail.com>
2021-06-07 20:17:50 +0200

    first foundation push

---
+++ readme.md
@@ -0,0 +1,9 @@
+# React is failing me
+
+## Time to Move on
+
+
+### Modern Day Picasso Web App
+
+- p2p(decentralized)
+- native web
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ['./src/**/*.html',
-    './src/**/*.js',],
+    './src/**/*.js',
+    './src/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
