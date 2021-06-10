6f1b7642aeec2b934e05c93c5ca0336d005cbd06
codebender <lionelmbankeu@gmail.com>
2021-06-10 19:56:56 +0200

    a full end to end with algorand

--- src/index.js
+++ src/index.js
@@ -46,6 +46,27 @@ const constructTransaction = async () => {
     let txn = algosdk.makePaymentTxnWithSuggestedParams(myAccount.addr, receiver, 1000000, undefined, note, params);
 
     console.log(txn)
+
+
+    //sign transaction
+
+    let signedTxn = txn.signTxn(myAccount.sk);
+    let txId = txn.txID().toString();
+    console.log("Signed transaction with txID: %s", txId);
+
+    //submit
+
+    await algodClient.sendRawTransaction(signedTxn).do();
+
+
+    // Wait for confirmation
+    let confirmedTxn = await waitForConfirmation(algodClient, txId, 4);
+    //Get the completed Transaction
+    console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+    let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
+    console.log("Transaction information: %o", mytxinfo);
+    var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+    console.log("Note field: ", string);
 }
 
 const checkStatus = async () => {
@@ -58,6 +79,53 @@ const checkSuggestedTransaction = async () => {
     let params = await algodClient.getTransactionParams().do();
     console.log("Algorand suggested parameters: %o", params)
 }
+
+
+/**
+ * Wait until the transaction is confirmed or rejected, or until 'timeout'
+ * number of rounds have passed.
+ * @param {algosdk.Algodv2} algodClient the Algod V2 client
+ * @param {string} txId the transaction ID to wait for
+ * @param {number} timeout maximum number of rounds to wait
+ * @return {Promise<*>} pending transaction information
+ * @throws Throws an error if the transaction is not confirmed or rejected in the next timeout rounds
+ */
+const waitForConfirmation = async function (algodClient, txId, timeout) {
+    if (algodClient == null || txId == null || timeout < 0) {
+        throw new Error("Bad arguments");
+    }
+
+    const status = (await algodClient.status().do());
+    if (status === undefined) {
+        throw new Error("Unable to get node status");
+    }
+
+    const startround = status["last-round"] + 1;
+    let currentround = startround;
+
+    while (currentround < (startround + timeout)) {
+        const pendingInfo = await algodClient.pendingTransactionInformation(txId).do();
+        if (pendingInfo !== undefined) {
+            if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+                //Got the completed Transaction
+                return pendingInfo;
+            } else {
+                if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
+                    // If there was a pool error, then the transaction has been rejected!
+                    throw new Error("Transaction " + txId + " rejected - pool error: " + pendingInfo["pool-error"]);
+                }
+            }
+        }
+        await algodClient.statusAfterBlock(currentround).do();
+        currentround++;
+    }
+
+    throw new Error("Transaction " + txId + " not confirmed after " + timeout + " rounds!");
+};
+
+
+
+
 
 // checkStatus()
 // checkSuggestedTransaction()
